bc53366ece8849ca581be7f244ce92e4d7bb876d
sunshners <sim28frvr@gmail.com>
2025-03-17 21:24:00 +0300

    task 2

--- src/homework_5/homework_5.java
+++ src/homework_5/homework_5.java
@@ -9,6 +9,7 @@ import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
 public class homework_5 {
     public static void main(String[]args){
         array();
+        chessBoard();
 
     }
     public static void array(){
@@ -49,6 +50,30 @@ public class homework_5 {
                     System.out.println(array[i][j][k]+ " ");
                 }
                 System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    // task 2
+    public static void chessBoard() {
+        String[][] chessBoard = new String[8][8];
+        int i;
+        for (i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0) {
+                    chessBoard[i][j] = "W";
+                } else {
+                    chessBoard[i][j] = "B";
+                }
+            }
+        }
+        printBoard(chessBoard);
+    }
+    public static void printBoard(String[][] board){
+        for (int i=0; i< board.length; i++){
+            for (int j=0; j< board.length; j++){
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
---
+++ src/lesson_6/lesson_6.java
@@ -0,0 +1,23 @@
+package lesson_6;
+
+public class lesson_6 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void startGame() {
+    }
+
+    public static void getMsg() {
+        System.out.println("welcome to guess world ");
+        System.out.println("yuo should guess the world.");
+    }
+
+    public static void playGame() {
+        System.out.println("*********************");
+        System.out.println();
+    }
+
+
+}
